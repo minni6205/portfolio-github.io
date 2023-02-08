6444118cc28a94a75fb6f0e435314c57aca1ffe4
minni6205 <115799420+minni6205@users.noreply.github.com>
2023-02-08 23:02:46 +0530

    portfolio website
    This is my portfolio website

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("script running...") 
+document.querySelector('.cross').style.display ='none'; 
+document.querySelector('.hamburger').addEventListener("click", ()=>{  
+    if(document.querySelector('.sidebar').classList.toggle('sidebarGo')){
+    document.querySelector('.ham').style.display ='inline' 
+    document.querySelector('.cross').style.display ='none'   
+    } 
+    else{ 
+        document.querySelector('.ham').style.display= 'none' 
+        setTimeout(()=> { 
+            document.querySelector('.cross').style.display= 'inline' 
+        }, 350);
+        }
+    })
